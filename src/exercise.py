f1b834a3f13ed3dec79f6d20ad1085341ae27016
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-17 04:19:42 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    num = int(input('How old are you?'))
+    
+    if num >= 18:
+        print('You are an adult')
+    else:
+        print('You are not an adult')
+    
+             
 
 if __name__ == '__main__':
     main()
